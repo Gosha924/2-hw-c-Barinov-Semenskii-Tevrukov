4454ab3290264c4ccab78a64113b216cd8f3ee67
Gosha924 <151758180+Gosha924@users.noreply.github.com>
2026-02-24 18:10:25 +0300

    убрал определение структур изbst.c

--- src/bst.c
+++ src/bst.c
@@ -2,17 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
-    int key;
-    struct node* left;
-    struct node* right;
-} Node;
-
-typedef struct bst {
-    Node* root;
-    int size;
-} BST;
-
 void bstInsert(BST* tree, int value)
 {
 }
