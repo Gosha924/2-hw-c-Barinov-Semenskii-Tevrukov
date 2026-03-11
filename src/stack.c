b192749b1fccb6b981b9b27a55ccea3fdc5ac702
Quark1111 <tevrukovila@gmail.com>
2026-03-11 23:09:26 +0300

    Update stack.c

--- src/stack.c
+++ src/stack.c
@@ -15,32 +15,31 @@ void stackPush(stack* Top, Node* node)
     stack* newElem = malloc(sizeof(stack));
     newElem->node = node;
     newElem->next = Top->next;
-    Top->next = newElem;    
+    Top->next = newElem;
 }
 
 void stackPop(stack* Top)
 {
     stack* elem = Top->next;
     if (elem != NULL) {
-	    Top->next = elem->next;
-	    free(elem);
+        Top->next = elem->next;
+        free(elem);
     }
 }
 
 Node* stackTop(stack* Top)
 {
     if (Top->next != NULL)
-	    return Top->next->node;
+        return Top->next->node;
     return NULL;
 }
-
 
 void stackFree(stack* Top)
 {
     while (Top->next != NULL) {
-	    stack* elem = Top->next;
-	    Top->next = elem->next;
-	    free(elem);
+        stack* elem = Top->next;
+        Top->next = elem->next;
+        free(elem);
     }
     free(Top);
 }
